c6c1ded92638dda621d5ce2f54d2e759442132cc
ismailhozain <ismailhozain@gmail.com>
2020-05-17 06:44:39 -0500

    added content, ported navbar, page setup complete.

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,13 +1,109 @@
 import React from 'react';
 import './landing.css';
-import styled from 'styled-components';
+import { Menu, Button } from 'antd';
+import 'antd/dist/antd.css';
+import {DownOutlined} from '@ant-design/icons';
 
+class Landing extends React.Component {
+    state = {
+        collapsed: false,
+    };
+    render() {
+        return (
+            <div>
+                <div className="App">
+                    <div className={"title section"}>
+                        <h1 className={"titleText"}>Ismail Hozain</h1>
+                        <Button shape={"circle"} type={"primary"}>
+                            <DownOutlined/>
+                        </Button>
+                    </div>
+                    <div className={"aboutSection section"}>
+                        <h1>About</h1>
+                        <p className={"bio"}>I am a freelance programmer and engineer. I mainly handle Web Development
+                            but am rapidly expanding my skills and continuing on 4 years of experience programming.</p>
+                        <h1>Education</h1>
+                        <p>Mckinney High School (Expected 2022)</p>
+                    </div>
+                    <div className={"skillsSection section"}>
+                        <h1>Skills</h1>
+                        <h2>Programming</h2>
+                        <ul>
+                            <li>Javascript</li>
+                            <li>React</li>
+                            <li>Ant Design</li>
+                            <li>Three.js</li>
+                            <li>Animejs</li>
+                            <li>Firebase</li>
+                            <li>Vue.js</li>
+                            <li>Python</li>
+                            <li>Selenium</li>
+                            <li>Java</li>
+                            <li>RobotC</li>
+                            <li>Arduino</li>
+                        </ul>
+                        <h2>Engineering</h2>
+                        <ul>
+                            <li>Industrial design in CAD</li>
+                            <li>Electronics and programming circuits in RobotC</li>
+                            <li>Pneumatics</li>
+                            <li>Modelmaking</li>
+                            <li>3d Printing</li>
+                            <li>Woodworking</li>
+                        </ul>
+                    </div>
+                    <div className={"faveTools section"}>
+                        <h1>Favorite tools</h1>
+                        <ul>
+                            <li>Webstorm + Pycharm</li>
+                            <li>Fusion 360</li>
+                            <li>Blender</li>
+                            <li>Wireshark</li>
+                            <li>Ghidra</li>
+                            <li>Burp Suite</li>
+                            <li>SQLMap</li>
+                            <li>John The Ripper</li>
+                            <li>Meshroom</li>
+                            <li>Chrome</li>
+                        </ul>
+                    </div>
+                    <div className={"achievementsSection section"}>
+                        <h1>Achievements</h1>
+                        <h2>TSA 2020</h2>
+                        <p>Disclaimer: Competition is still going, these are interim results and qualifications</p>
+                        <ul>
+                            <li>1st Place Regional(NC.TX) - Transportation Modeling</li>
+                        </ul>
+                        <h2>TSA 2019</h2>
+                        <ul>
+                            <li>6th Place Nationals - Cybersecurity</li>
+                            <li>2nd Place State(TX) - Transportation Modeling</li>
+                            <li>1st Place State(TX) - Software Development</li>
+                            <li>2nd Place State(TX) - Systems Control</li>
+                        </ul>
+                    </div>
+                    <div className={"projectSection section"}>
+                        <h1>Projects</h1>
+                        <ul>
+                            <li>WebIDE</li>
+                            <li>Maglev Train Model</li>
+                            <li>Random and Organized Chaos</li>
+                            <li>More are in the works...</li>
+                        </ul>
+                    </div>
+                    <div className={"contactSection section"}>
+                        <h1>Find Me in these Places</h1>
+                        <ul>
+                            <li>Github</li>
+                            <li>Email</li>
+                            <li>Codepen</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
-const Landing = () => (
-  <div className={"titleDiv"}>
-    <h1 className={"title"}>Your app goes here!</h1>
-
-  </div>
-);
 
 export default Landing;
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
-import {Menu} from "antd";
+import {Button, Menu} from "antd";
 import 'antd/dist/antd.css';
 import Logo from './logo.png';
 import './navigation.css';
+import {
+    DashboardOutlined, DesktopOutlined, InboxOutlined, ProjectOutlined, ToolOutlined, TrophyOutlined, UnlockOutlined
+} from "@ant-design/icons";
 const NavigationWrapper = () => (
     <div>
        <Navigation/>
@@ -12,10 +15,40 @@ const NavigationWrapper = () => (
 );
 
 const Navigation = () => (
-    <Menu mode={"horizontal"}>
-         <Menu.Item>
-             <Link to={ROUTES.LANDING}><img alt = "the logo of this site. This is the logo of Ismail Hozain, circa 2018, a black dragon surrounding his initials I and H" src={Logo}/></Link>
-        </Menu.Item>
-    </Menu>
+    <div className={"menuClass"}>
+        <Menu
+            defaultSelectedKeys={['1']}
+            mode="horizontal"
+            theme="dark">
+            <Menu.Item key="1">
+                <DashboardOutlined />
+                <span>Home</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+                <DesktopOutlined />
+                <span>About</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+                <UnlockOutlined />
+                <span>Skills</span>
+            </Menu.Item>
+            <Menu.Item key="4">
+                <ToolOutlined />
+                <span>Tools</span>
+            </Menu.Item>
+            <Menu.Item key="5">
+                <TrophyOutlined />
+                <span>Achievements</span>
+            </Menu.Item>
+            <Menu.Item key="6">
+                <ProjectOutlined />
+                <span>Projects</span>
+            </Menu.Item>
+            <Menu.Item key="7">
+                <InboxOutlined />
+                <span>Contact</span>
+            </Menu.Item>
+        </Menu>
+    </div>
 );
 export default NavigationWrapper;
