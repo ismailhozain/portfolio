43e4e3921e3cebf5afc3f05d013de3eede4bed33
Ismail Hozain <36ismailhozain@celinaisd.com>
2020-05-17 06:08:48 -0500

    Initial commit

---
+++ src/components/Landing/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './landing.css';
+import styled from 'styled-components';
+
+
+const Landing = () => (
+  <div className={"titleDiv"}>
+    <h1 className={"title"}>Your app goes here!</h1>
+
+  </div>
+);
+
+export default Landing;
